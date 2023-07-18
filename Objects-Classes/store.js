33aaa863c4087f1d34adc977ccbf9d4329aaf679
rstoyanovv <115493787+rstoyanovv@users.noreply.github.com>
2023-07-18 22:40:50 +0300

    Add files via upload

---
+++ Objects-Classes/store.js
@@ -0,0 +1,25 @@
+function store (stock, ordered){
+    let storedProducts = {};
+    let store;
+    for(let i = 0; i < stock.length; i+=2){
+        let currentProduct = stock[i];
+        storedProducts[currentProduct] = parseInt(stock[i+1]);
+    }
+
+    for(let i = 0; i < ordered.length; i+=2){
+        let currentProduct = ordered[i];
+        if(!storedProducts.hasOwnProperty(currentProduct)){
+            storedProducts[currentProduct] = 0;
+
+        }
+        storedProducts[currentProduct] += parseInt(ordered[i+1]);
+    }
+
+    for (let product in storedProducts) {
+        console.log(`${product} -> ${storedProducts[product]}`);
+    }
+     
+}
+
+store(["Chips", "5", "Apples", "12", "Bananas", "15"],
+["Beer", "6", "Bananas", "7", "Tomatoes", "8"]);
